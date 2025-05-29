6ee4b866e30f054cc18211a51ab8ae51018230b8
Gesart Mlloja <gesmll20@htl-shkoder.com>
2025-05-29 11:03:56 +0200

    gesart 1.commit

---
+++ src/main/java/com/example/projekt_sew_3/DifficultyManager.java
@@ -0,0 +1,15 @@
+package com.example.projekt_sew_3;
+
+import javafx.scene.control.ComboBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
+import javafx.scene.text.Font;
+import javafx.scene.paint.Color;
+public class DifficultyManager {
+    private static final int WIDTH = 600;
+    private static final int HEIGHT = 400;
+
+    private ComboBox<String> difficultySelector;
+    private Text difficultyLabel;
+    private Pane root;
+}
