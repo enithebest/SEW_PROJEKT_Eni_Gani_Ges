cd3bd2d8fcbc5be89b1ab483c24be8116fc4431a
Gesart Mlloja <gesmll20@htl-shkoder.com>
2025-05-29 11:43:39 +0200

    gesart 13.commit

---
+++ src/main/java/com/example/projekt_sew_3/ObstacleManager.java
@@ -0,0 +1,12 @@
+package com.example.projekt_sew_3;
+
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.paint.Color;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class ObstacleManager {
+
+}
